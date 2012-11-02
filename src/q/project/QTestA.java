5879acc8b015abd3ae88b85140bf3cf08e5ff6ff
Administrator <Administrator@admin-PC>
2012-11-02 18:10:54 +0800

    Q

--- src/q/project/QTestA.java
+++ src/q/project/QTestA.java
@@ -1,16 +1,10 @@
 package q.project;
 
-import qv.web.oauth.OnWvOauthListener;
-import qv.web.oauth.WvOauthEntity;
-import qv.web.oauth.WvOauthHandleSinaWeibo;
-import qv.web.oauth.WvOauthUtil;
-
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
-public class QTestA extends Activity implements OnWvOauthListener {
+public class QTestA extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,29 +13,6 @@ public class QTestA extends Activity implements OnWvOauthListener {
 		super.onCreate(savedInstanceState);
 		WebView web = new WebView(this);
 		setContentView(web);
-		WvOauthUtil.init(new WvOauthEntity(0, this, web, new WvOauthHandleSinaWeibo()));
-	}
-
-	@Override
-	public void onWvOauthLoadingFinish(WvOauthEntity en) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onWvOauthAuthing(WvOauthEntity en) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onWvOauthSuccess(WvOauthEntity en) {
-		System.out.println(en.getToken().getToken());
-	}
-
-	@Override
-	public void onWvOauthError(WvOauthEntity en) {
-		// TODO Auto-generated method stub
 		
 	}
 
